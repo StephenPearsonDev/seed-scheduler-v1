0db3f687867cb905c0aaa9a87ce1dbd457dea13b
Stephen <pearsonsw@gmail.com>
2023-12-14 09:50:13 +0100

    implementing active search for varieties field

--- src/main/java/co/uk/stephenpearson/seedschedulerv1/dashboard/DashBoardController.java
+++ src/main/java/co/uk/stephenpearson/seedschedulerv1/dashboard/DashBoardController.java
@@ -1,7 +1,11 @@
 package co.uk.stephenpearson.seedschedulerv1.dashboard;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DashBoardController {
@@ -10,5 +14,32 @@ public class DashBoardController {
 	public String getDashBoard() {
 		return "dashboard";
 	}
+	
+	@GetMapping("/scheduler")
+	public String getScheduler() {
+		return "scheduler";
+	}
+	
+	@GetMapping("/api/varieties")
+	@ResponseBody
+	public String getVarieties(@RequestParam(required = false) String query) {
+	    System.out.println("Searching: " + query);
+	    List<String> allVarieties = List.of("Clapton F1", "Gucio F1", "Crimson Crush");
+
+	    if (query == null || query.isEmpty()) {
+	        return "";
+	    }
+
+	    return allVarieties.stream()
+	                       .filter(variety -> variety.toLowerCase().startsWith(query.toLowerCase()))
+	                       .findFirst()
+	                       .orElse("");
+	}
+
+
+
+
+
+
 
 }
--- src/main/java/co/uk/stephenpearson/seedschedulerv1/seedpack/SeedPack.java
+++ src/main/java/co/uk/stephenpearson/seedschedulerv1/seedpack/SeedPack.java
@@ -1,5 +1,7 @@
 package co.uk.stephenpearson.seedschedulerv1.seedpack;
 
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +16,6 @@ public class SeedPack {
     private String subcategory;
     private String variety;
     private String notes;
+    private int sowingDay;
+    
 }
